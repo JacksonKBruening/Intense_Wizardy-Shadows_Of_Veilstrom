baa5f857bcbcb6ea62b178053434e09dfeb02411
Jackson Bruening <85266719+JacksonKBruening@users.noreply.github.com>
2021-06-20 18:28:40 -0500

    Add files via upload

---
+++ Controllers/CharactersDatabaseController.cs
@@ -0,0 +1,29 @@
+﻿using IntenseWizardy_ShadowsOfVeilstorm.Models;
+using IntenseWizardy_ShadowsOfVeilstorm.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntenseWizardy_ShadowsOfVeilstorm.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CharactersDatabaseController : ControllerBase
+    {
+        public CharactersDatabaseController(JsonFileCharactersService charactersService)
+        {
+            this.CharactersService = charactersService;
+        }
+
+        public JsonFileCharactersService CharactersService { get; }
+
+        [HttpGet]
+        public IEnumerable<NPCharacters> Get()
+        {
+            return CharactersService.GetCharacters();
+        }
+    }
+}
---
+++ Models/NPCharacters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IntenseWizardy_ShadowsOfVeilstorm.Models
+{
+    public class NPCharacters
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Race { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public string Backstory { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<NPCharacters>(this);
+    }
+}
